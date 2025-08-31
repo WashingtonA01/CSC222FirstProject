c1306e14a3203bec811a5de034ce56f8aa4b0396
WashingtonA01 <arianna.s.washington@gmail.com>
2025-08-31 16:58:57 -0400

    Add project files.

---
+++ CSC222FirstProject.cpp
@@ -0,0 +1,15 @@
+// CSC222FirstProject.cpp
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    char first, middle, last;
+    first = 'A';
+    middle = 'S';
+    last = 'W';
+    cout << first << middle << last << endl;
+    return 0;
+
+}
